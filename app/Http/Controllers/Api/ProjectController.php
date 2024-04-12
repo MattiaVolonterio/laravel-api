7910c8f3a0264970367799cd55f262898261c116
MattiaVolonterio <volonteriomattia@gmail.com>
2024-04-12 16:25:55 +0200

    feat: api and projectapi controller

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::select(['id', 'type_id', 'title', 'author', 'description', 'project_link', 'project_image'])
+            ->with(['type:id,type,color', 'technologies:id,type,color'])
+            ->paginate(5);
+
+        foreach ($projects as $project) {
+            $project->project_image = !empty($project->project_image) ? asset('/storage/' . $project->project_image) : 'https://placehold.co/600x400';
+        }
+
+        return response()->json($projects);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
